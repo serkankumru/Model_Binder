0990d8dd8d358df933d75225f4e595b354a3595a
Serkan Kumru <serkankumru1997@gmail.com>
2023-09-14 13:25:27 +0300

    Add files via upload

---
+++ Global.asax.cs
@@ -0,0 +1,48 @@
+﻿using Autofac;
+using Autofac.Integration.Mvc;
+using DAL;
+using DAL.Repository;
+using News.MyModelBinders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace News
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+
+            //Model Binders
+            ModelBinders.Binders.Add(typeof(NewsT),new NewModelBinder());
+            
+            
+            //MyControllerFactory myController = new MyControllerFactory();
+            //ControllerBuilder.Current.SetControllerFactory(myController);
+
+
+            var builder = new ContainerBuilder();
+
+            builder.RegisterControllers(Assembly.GetExecutingAssembly());
+            builder.RegisterType<EditorRepository>().As<EditorRepository>();
+            builder.RegisterType<NewsRepository>().As<NewsRepository>();
+            builder.RegisterType<ImageRepository>().As<ImageRepository>();
+            builder.RegisterType<CategoryRepository>().As<CategoryRepository>();
+
+            IContainer container = builder.Build();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+
+        }
+    }
+}
---
+++ NewModelBinder.cs
@@ -0,0 +1,31 @@
+﻿using DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace News.MyModelBinders
+{
+    public class NewModelBinder:DefaultModelBinder
+    {
+        public override object BindModel(ControllerContext controllerContext, ModelBindingContext bindingContext)
+        {
+            if(bindingContext.ModelType == typeof(NewsT))
+            {
+                var request = controllerContext.HttpContext.Request;
+                int id = Convert.ToInt32(request.QueryString["id"]);
+                var ctx = new NewsEntities();
+                NewsT model = ctx.NewsT.FirstOrDefault(x=>x.Id==id);
+                if(model==null)
+                    return base.BindModel(controllerContext, bindingContext);
+
+                return model;
+            }
+            else
+            {
+                return base.BindModel(controllerContext, bindingContext);
+            }
+        }
+    }
+}
